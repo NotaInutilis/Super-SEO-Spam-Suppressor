731dbdd94b69bc11ac782de0e36b30aed229f66f
Hugo <hugo@notainutilis.fr>
2023-10-26 20:12:40 +0200

    sources

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,8 @@
 # ./scripts/update.sh
 
 # Cleanup sources:
+## Special cleanup for imported sources of other formats (AdBlock, hosts, etc.).
+find ./sources/_imported -type f -name "*.txt" -exec sed -ri 's/^[^#[:alnum:]]/#&/; s/^0\.0\.0\.0[[:space:]]*//i' {} \;
 ## Normalizes URLs into domains: lowercases, remove leading spaces, protocol (`x://`) `www.` subdomains, everything after `/`, only one space before `#`. Keeps comments intact.
 find ./sources -type f -name "*.txt" -exec sed -ri 'h; s/[^#]*//1; x; s/#.*//; s/.*/\L&/; s/^[[:space:]]*//i; s/^.*:\/\///i; s/^[.*]*//i; s/^www\.//i; s/\/[^[:space:]]*//i; s/[[:space:]].*$/ /i; G; s/(.*)\n/\1/' {} \;
 ## Remove duplicate domains from each source file (keeps repeated comments and empty lines for organization).
