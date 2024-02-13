4b55743b9dc2075922e3f1478f4502f4befe6c97
Hugo <hugo@notainutilis.fr>
2024-02-13 22:02:19 +0100

    will this work? will this break?

---
+++ scripts/adblock.py
@@ -0,0 +1,23 @@
+# Generates a list formatted for ad blocking browser extensions from the content of the `sources` folder.
+# Usage:
+# python adblock.py > adblock.txt
+
+# Open header
+with open("sources/headers/adblock.txt", "r") as header:
+	linesheader = header.readlines()
+
+# Open blocked formats
+with open("sources/domains.txt", "r") as domains:
+	linesdomains = domains.readlines()
+with open("sources/tlds.txt", "r") as tlds:
+	linestlds = tlds.readlines()
+with open("sources/urls.txt", "r") as urls:
+	linesurls = urls.readlines()
+blocklist = linesdomains + linestlds + linesurls
+
+# Print blocklist
+for line in header:
+	print(line)
+print()
+for line in blocklist:
+	print('||' + line.strip() + '^')
